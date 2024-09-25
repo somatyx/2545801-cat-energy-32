1a4970ea21a72361b3f450dd55a35118f90756a9
Pavel Kuznetsov <pavel.kuznetsov.alexandrovich@gmail.com>
2024-09-25 19:21:27 +0300

    add-adaptive

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+const switchClass = function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+};
+
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', switchClass);
